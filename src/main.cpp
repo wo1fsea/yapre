069f7cfb976eb2d38d08e6c163c859fef6627e0a
wo1fsea <wo1fsea@126.com>
2021-02-25 21:04:30 +0800

    mod emsripten with SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
@@ -6,60 +10,71 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+void main_loop()
+{
+    return;
+}
+
 int main(int argc, char *args[])
 {
-	//The window we'll be rendering to
-	SDL_Window *window = NULL;
+    //The window we'll be rendering to
+    SDL_Window *window = NULL;
 
-	//The surface contained by the window
-	SDL_Surface *screenSurface = NULL;
+    //The surface contained by the window
+    SDL_Surface *screenSurface = NULL;
 
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			printf("TA");
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            printf("TA");
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
 
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
 
-			//Wait two seconds
-			bool quit = false;
-			SDL_Event e;
-			while (!quit)
-			{
-				while (SDL_PollEvent(&e))
-				{
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-				}
-			}
-		}
-	}
+            //Wait two seconds
+            bool quit = false;
+            SDL_Event e;
 
-	//Destroy window
-	SDL_DestroyWindow(window);
+#ifdef __EMSCRIPTEN__
+            emscripten_set_main_loop(main_loop, 60, 1);
+#else
+            while (!quit)
+            {
+                while (SDL_PollEvent(&e))
+                {
+                    if (e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+            }
+#endif
 
-	//Quit SDL subsystems
-	SDL_Quit();
+        }
+    }
 
-	return 0;
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
 }
