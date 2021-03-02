b13d9e5111c30e863271b185fca56c6e3e5d0654
wo1fsea <wo1fsea@126.com>
2021-03-02 11:57:40 +0800

    add model

--- src/core.cpp
+++ src/core.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 #include <SDL.h>
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <glad/glad.h>
 
 #include "camera.h"
@@ -126,6 +126,40 @@ namespace yapre
 
         void Deinit() {}
 
+        void RenderFrame() {
+            // render
+            // ------
+            glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // don't forget to enable shader before setting uniforms
+            ourShader->use();
+
+            // view/projection transformations
+            glm::mat4 projection =
+                glm::perspective(glm::radians(camera.Zoom),
+                        (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            ourShader->setMat4("projection", projection);
+            ourShader->setMat4("view", view);
+
+            // render the loaded model
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(
+                    model,
+                    glm::vec3(0.0f, 0.0f,
+                        0.0f)); // translate it down so it's at the center of the scene
+            model = glm::scale(
+                    model,
+                    glm::vec3(1.0f, 1.0f,
+                        1.0f)); // it's a bit too big for our scene, so scale it down
+            ourShader->setMat4("model", model);
+            ourModel->Draw(*ourShader);
+
+            SDL_GL_SwapWindow(window);
+        }
+
+
         bool Update() {
             SDL_Event event;
             SDL_PollEvent(&event);
@@ -162,43 +196,9 @@ namespace yapre
                         break;
                 }
             }
-            
+
             RenderFrame();
             return event.type != SDL_QUIT;
         }
-
-        void RenderFrame() {
-            // render
-            // ------
-            glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            // don't forget to enable shader before setting uniforms
-            ourShader->use();
-
-            // view/projection transformations
-            glm::mat4 projection =
-                glm::perspective(glm::radians(camera.Zoom),
-                        (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-            glm::mat4 view = camera.GetViewMatrix();
-            ourShader->setMat4("projection", projection);
-            ourShader->setMat4("view", view);
-
-            // render the loaded model
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(
-                    model,
-                    glm::vec3(0.0f, 0.0f,
-                        0.0f)); // translate it down so it's at the center of the scene
-            model = glm::scale(
-                    model,
-                    glm::vec3(1.0f, 1.0f,
-                        1.0f)); // it's a bit too big for our scene, so scale it down
-            ourShader->setMat4("model", model);
-            ourModel->Draw(*ourShader);
-
-            SDL_GL_SwapWindow(window);
-        }
-
     } // namespace pinhole
 }
