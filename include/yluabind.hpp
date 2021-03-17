590cc19fa64b23b0be1850bcd7d83dfa13dde73e
wo1fsea <wo1fsea@126.com>
2021-03-17 22:47:20 +0800

    add lua bind

---
+++ include/yluabind.hpp
@@ -0,0 +1,143 @@
+extern "C" {
+#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
+}
+
+#include <string>
+#include <string_view>
+
+namespace yapre {
+namespace lua {
+
+extern lua_State *mainLuaState;
+
+template <typename T> struct StateVar {
+  static inline void Put(lua_State *l, T value);
+  static inline T Get(lua_State *l, int index);
+};
+
+template <> struct StateVar<void> {
+  static inline void Put(lua_State *l) { lua_pushnil(l); }
+};
+
+template <> struct StateVar<bool> {
+  static inline void Put(lua_State *l, bool value) {
+    lua_pushboolean(l, value);
+  }
+  static inline bool Get(lua_State *l, int index) {
+    return lua_toboolean(l, index);
+  }
+};
+
+template <> struct StateVar<int> {
+  static inline void Put(lua_State *l, int value) { lua_pushinteger(l, value); }
+  static inline int Get(lua_State *l, int index) {
+    return luaL_checkinteger(l, index);
+  }
+};
+
+template <> struct StateVar<float> {
+  static inline void Put(lua_State *l, float value) {
+    lua_pushnumber(l, value);
+  }
+  static inline float Get(lua_State *l, int index) {
+    return (float)luaL_checknumber(l, index);
+  }
+};
+
+template <> struct StateVar<double> {
+  static inline void Put(lua_State *l, double value) {
+    lua_pushnumber(l, value);
+  }
+  static inline double Get(lua_State *l, int index) {
+    return (double)luaL_checknumber(l, index);
+  }
+};
+
+template <> struct StateVar<std::string> {
+  static inline void Put(lua_State *l, std::string value) {
+    lua_pushstring(l, value.c_str());
+  }
+  static inline std::string Get(lua_State *l, int index) {
+    return luaL_checkstring(l, index);
+  }
+};
+
+template <size_t args_size, typename R, typename... Targs> struct _StateCall {};
+
+template <typename R, typename... Targs> struct StateCall {
+  static inline R Call(lua_State *l, Targs... args) {
+    return _StateCall<sizeof...(Targs), R, Targs...>::Call(l, args...);
+  }
+};
+
+template <size_t args_size, typename R, typename T, typename... Targs>
+struct _StateCall<args_size, R, T, Targs...> {
+  static inline R Call(lua_State *l, T t, Targs... args) {
+    StateVar<T>::Put(t);
+    return _StateCall<sizeof...(Targs), R, Targs...>::Call(l, args...);
+  }
+};
+
+template <size_t args_size, typename R> struct _StateCall<args_size, R> {
+  static inline R Call(lua_State *l) {
+    // todo check pcall
+    lua_pcall(l, args_size, 1, 0);
+    R result = StateVar<R>::Get(l, -1);
+    lua_pop(l, 1);
+    return result;
+  }
+};
+
+template <size_t args_size> struct _StateCall<args_size, void> {
+  static inline void Call(lua_State *l) {
+    // todo check pcall
+    lua_pcall(l, args_size, 0, 0);
+  }
+};
+
+template <typename R, typename... Targs> struct GStateCall {
+  static inline R Call(Targs... args) {
+    return StateCall<R, Targs...>::Call(mainLuaState, args...);
+  }
+};
+
+template <typename R, typename... Targs> struct StateFunc {
+  std::string functionName;
+  R Call(lua_State *l, Targs... args) {
+    lua_getglobal(l, functionName.c_str());
+    return StateCall<R, Targs...>::Call(l, args...);
+  }
+};
+
+template <typename R, typename... Targs> struct GStateFunc {
+  std::string functionName;
+  R Call(Targs... args) {
+    lua_getglobal(mainLuaState, functionName.c_str());
+    return StateCall<R, Targs...>::Call(mainLuaState, args...);
+  }
+};
+
+template <typename R> struct StateCall<R, void> {
+  static inline R Call(lua_State *l) { return _StateCall<0, R>::Call(l); }
+};
+
+template <typename R> struct StateFunc<R, void> {
+  std::string functionName;
+  R Call(lua_State *l) {
+    lua_getglobal(l, functionName.c_str());
+    return StateCall<R>::Call(l);
+  }
+};
+
+template <typename R> struct GStateFunc<R, void> {
+  std::string functionName;
+  R Call() {
+    lua_getglobal(mainLuaState, functionName.c_str());
+    return StateCall<R>::Call(mainLuaState);
+  }
+};
+
+} // namespace lua
+} // namespace yapre
--- src/ylua.cpp
+++ src/ylua.cpp
@@ -6,133 +6,31 @@ extern "C" {
 #include "lualib.h"
 }
 
-#include <string_view>
-#include <string>
+#include "yluabind.hpp"
 
 namespace yapre {
-    namespace lua {
+namespace lua {
 
-        lua_State *luaState = nullptr;
-        
-        template <typename T> 
-        struct State{
-            static inline void Put(T value);
-            static inline T Get(int index);
-        };
-        
-        template <> 
-        struct State<void>{
-            static inline void Put(){ lua_pushnil(luaState); }
-        };
+lua_State *mainLuaState = nullptr;
 
-        template <> 
-        struct State<bool>{
-            static inline void Put(bool value){ luaL_checkinteger(luaState, value); }
-            static inline bool Get(int index){ return luaL_checkinteger(luaState, index) == 0; }
-        };
+void LoadLuaFile(const std::string &luaFilePath) {
+  luaL_dofile(mainLuaState, luaFilePath.c_str());
+}
 
-        template <> 
-        struct State<int>{
-            static inline void Put(int value){ lua_pushinteger(luaState, value); }
-            static inline int Get(int index){ return luaL_checkinteger(luaState, index); }
-        };
+bool Init() {
+  mainLuaState = luaL_newstate();
+  luaL_openlibs(mainLuaState);
+  LoadLuaFile("data/lua/yapre.lua");
 
-        template <> 
-        struct State<float>{
-            static inline void Put(float value){ lua_pushnumber(luaState, value); }
-            static inline float Get(int index){ return (float)luaL_checknumber(luaState, index); }
-        };
+  return GStateFunc<bool, void>{"Init"}.Call();
+}
 
-        template <> 
-        struct State<double>{
-            static inline void Put(double value){ lua_pushnumber(luaState, value); }
-            static inline double Get(int index){ return (double)luaL_checknumber(luaState, index); }
-        };
+void Deinit() {
+  GStateFunc<void>{"Deinit"}.Call();
+  lua_close(mainLuaState);
+}
 
-        template <> 
-            struct State<std::string>{
-                static inline void Put(std::string value){ lua_pushstring(luaState, value.c_str());  }
-                static inline std::string Get(int index){ return luaL_checkstring(luaState, index); }        
-            };
-        
-        template <typename R, typename... Targs> 
-        struct StateFunc
-        {
-            static R Call(Targs... args)
-            {
-                return _StateFunc<sizeof...(Targs), R, Targs...>::Call(args...);
-            }
-        };
+void Update() { GStateFunc<void, void>{"Update"}.Call(); }
 
-        template <size_t args_size, typename R, typename... Targs> 
-        struct _StateFunc{};
-        
-        template <size_t args_size, typename R, typename T, typename... Targs> 
-        struct _StateFunc<args_size, R, T, Targs...>
-        {
-            static R Call(T t, Targs... args)
-            {
-                State<T>::Put(t);
-                return _StateFunc<sizeof...(Targs), R, Targs...>::Call(args...);
-            }
-        };       
-               
-        template <size_t args_size, typename R> 
-        struct _StateFunc<args_size, R>
-        {
-            static R Call()
-            {
-                lua_pcall(luaState, args_size, 1, 0);
-                return State<R>::Get(-1);
-            }
-        };
-/*
-        template <typename... Targs> 
-        struct StateFunc<void, typename... Targs>
-        {
-            static void Call(Targs... args)
-            {
-                 
-            }
-        }; 
-*/
-        void LoadLuaFile(const std::string& luaFilePath)
-        {
-            luaL_dofile(luaState, luaFilePath.c_str());
-        }
-
-        bool CallLuaInit()
-        {
-            lua_getglobal(luaState, "Init");
-            StateFunc<bool, int>::Call(1);
-            lua_pcall(luaState, 0, 1, 0);
-            bool result = (bool)lua_toboolean(luaState, -1);
-            lua_pop(luaState, 1);
-
-            return result;
-        }
-
-        bool Init() {
-            luaState = luaL_newstate();
-            luaL_openlibs(luaState);
-
-            LoadLuaFile("data/lua/yapre.lua");
-
-            return CallLuaInit();
-        }
-
-        void Deinit() 
-        {
-            lua_getglobal(luaState, "Deinit");
-            lua_pcall(luaState, 0, 0, 0);
-            lua_close(luaState);
-        }
-
-        void Update() 
-        {
-            lua_getglobal(luaState, "Update");
-            lua_pcall(luaState, 0, 0, 0);
-        }
-
-    } // namespace lua
+} // namespace lua
 } // namespace yapre
