e0e760e38a6e8e8e5cd200fa80818aecb4cb8e88
wo1fsea <wo1fsea@126.com>
2021-04-10 14:47:50 +0800

    mod: yecs

--- data/lua/yecs.lua
+++ data/lua/yecs.lua
@@ -1,41 +1,91 @@
-local yecs 
+local yecs = {}
 
-local 
+yecs.worlds = {}
 
-local __worldMeta = 
-{
-    __index = {
-        add = tiny.add,
-        addEntity = tiny.addEntity,
-        addSystem = tiny.addSystem,
-        remove = tiny.remove,
-        removeEntity = tiny.removeEntity,
-        removeSystem = tiny.removeSystem,
-        refresh = tiny.refresh,
-        update = tiny.update,
-        clearEntities = tiny.clearEntities,
-        clearSystems = tiny.clearSystems,
-        getEntityCount = tiny.getEntityCount,
-        getSystemCount = tiny.getSystemCount,
-        setSystemIndex = tiny.setSystemIndex
-    },
-    __tostring = function()
-        return "<yecs-world>"
-    end
+local World = {
+    -- key = "",
+    -- entities = {},
+    -- systems = {},
+    __metatable = "WorldMeta",
+    __tostring = function(self) return string.format("<yecs-world: %s>", self.key) end,
 }
 
-yecs.NewWorld = function(name)
-    local world = setmetatable(
+function World:New(key)
+    local world = yecs.worlds[key]
+    if(world)
+    then
+        return world
+    end
+
+    world = setmetatable(
     {
+        key = key,
         entities = {},
         systems = {},
-    }, 
-    __worldMeta
+    },
+    self 
     )
-
+    yecs.worlds[key] = world
     return world
 end
 
+function World:Destroy()
+   yecs.worlds[self.key] = nil 
+end
+
+function World:Update(ms)
+    for key, system in pairs(self.systems) do
+        system.Update(ms)
+    end
+end
+
+function World:AddEntity(entity)
+    self.entities[entity.key] = entity
+end
+
+function World:RemoveEntity(entity)
+    self.entities[entity.key] = nil
+end
+
+function World:AddSystem(system)  
+    self.systems[system.key] = system
+end
+
+function World:RemoveSystem(system)
+    self.systems[system.key] = nil 
+end
+
+
+-- entity
+
+local entity_key = 0
+local Entity = {
+    key = "",
+    __metatable = "EntityMeta",
+    __tostring = function(self) return string.format("<yecs-world: %s>", self.name) end,
+
+}
+
+function Entity:New(components={})
+    entity_key += 1
+    local entity = setmetatable(
+    {
+        key = entity_key,
+        components = components
+        __index = components,
+    },
+    self 
+    )
+
+    return entity
+end
+
+function Entity:AddComponent(key, component)
+    self.components[key] = components
+end
+
+
+local System = {}
 
 
 return yecs
