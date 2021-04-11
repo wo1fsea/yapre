5e643de2dd7d61dbf0fda0e9f8e93bedf4ceb3bb
wo1fsea <wo1fsea@126.com>
2021-04-11 13:02:10 +0800

    mod: yecs

--- data/lua/yecs.lua
+++ data/lua/yecs.lua
@@ -1,30 +1,42 @@
 local yecs = {}
 
+function deep_copy(obj, seen)
+	-- Handle non-tables and previously-seen tables.
+	if type(obj) ~= 'table' then return obj end
+	if seen and seen[obj] then return seen[obj] end
+
+	-- New table; mark it as seen an copy recursively.
+	local s = seen or {}
+	local res = {}
+	s[obj] = res
+	for k, v in next, obj do res[deep_copy(k, s)] = deep_copy(v, s) end
+	return setmetatable(res, getmetatable(obj))
+end
+
+-- world
+
 yecs.worlds = {}
 
 local World = {
-    -- key = "",
-    -- entities = {},
-    -- systems = {},
     __metatable = "WorldMeta",
     __tostring = function(self) return string.format("<yecs-world: %s>", self.key) end,
 }
+World.__index = World
 
 function World:New(key)
     local world = yecs.worlds[key]
-    if(world)
-    then
-        return world
-    end
+    if world then return world end
 
     world = setmetatable(
     {
         key = key,
         entities = {},
         systems = {},
+        system_update_queue=setmetatable({}, {__mode="v"}),
     },
     self 
     )
+    
     yecs.worlds[key] = world
     return world
 end
@@ -34,7 +46,7 @@ function World:Destroy()
 end
 
 function World:Update(ms)
-    for key, system in pairs(self.systems) do
+    for _, system in ipairs(self.system_update_queue) do
         system.Update(ms)
     end
 end
@@ -49,10 +61,13 @@ end
 
 function World:AddSystem(system)  
     self.systems[system.key] = system
+    table.insert(self.system_update_queue, system)
+    table.sort(self.system_update_queue, function(s1, s2) return s1.update_order < s2.update_order end)
 end
 
 function World:RemoveSystem(system)
     self.systems[system.key] = nil 
+    collectgarbage()
 end
 
 
@@ -62,17 +77,29 @@ local entity_key = 0
 local Entity = {
     key = "",
     __metatable = "EntityMeta",
-    __tostring = function(self) return string.format("<yecs-world: %s>", self.name) end,
-
+    __tostring = function(self) return string.format("<yecs-entity: %s>", self.key) end,
 }
+Entity.__index = function(self, k) return Entity[k] or self.components[k] end
 
-function Entity:New(components={})
-    entity_key += 1
+function Entity:New(components)
+    components = components or {}
+    local component_data = {}
+   
+    component_keys = {}
+    for k, v in pairs(components) do
+        if getmetatable(v) ~= "ComponentMeta" then
+            print(v, yecs.Component:New(v) )
+            component_data[v] = yecs.Component:New(v) 
+        else
+            component_data[v.key] = v 
+        end
+    end
+
+    entity_key = entity_key + 1
     local entity = setmetatable(
     {
         key = entity_key,
-        components = components
-        __index = components,
+        components = component_data,
     },
     self 
     )
@@ -80,12 +107,74 @@ function Entity:New(components={})
     return entity
 end
 
-function Entity:AddComponent(key, component)
-    self.components[key] = components
+function Entity:AddComponent(component)
+    if getmetatable(component) ~= "ComponentMeta" then
+        component = yecs.Component:New(component)
+    end
+     
+    if component and self.components[component.key] == nil then
+        self.components[component.key] = component
+    end
 end
 
+-- component
+local component_templates = {}
 
-local System = {}
+local Component = {
+    key = "",
+    __metatable = "ComponentMeta",
+    __tostring = function(self) return string.format("<yecs-component: %s>", self.key) end,
+}
+Component.__index = Component
 
+function Component:Register(key, data)
+	if type(data) ~= 'table' then return end
+    data["key"] = key
+    component_templates[key] = data
+end
 
+function Component:New(key)
+    local component_data = component_templates[key]
+    if component_data == nil then return nil end
+
+    local component = setmetatable
+    (
+       deep_copy(component_data),
+       self
+    )
+    return component
+end
+
+-- system
+local system_templates = {}
+local System = {
+    key = "",
+    update_order = 1024,
+    __metatable = "SystemMeta",
+    __tostring = function(self) return string.format("<yecs-system: %s>", self.key) end,
+}
+System.__index = System
+
+function System:Register(key, data)
+	if type(data) ~= 'table' then return end
+    data["key"] = key
+    system_templates[key] = data
+end
+
+function System:New(key)
+    system_data = system_templates[key]
+    if system_data == nil then return nil end
+
+    local system = setmetatable
+    (
+       deep_copy(system_data),
+       self
+    )
+    return system
+end
+
+yecs.World = World
+yecs.Entity = Entity
+yecs.Component = Component 
+yecs.System = System
 return yecs
