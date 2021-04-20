3115ea0ab7ad3c4f5af6e409a735fc147fea56bb
wo1fsea <wo1fsea@126.com>
2021-04-20 21:23:03 +0800

    mod: render to buffer

--- src/yrenderer.cpp
+++ src/yrenderer.cpp
@@ -23,8 +23,13 @@
 namespace yapre {
 namespace renderer {
 
+GLuint FramebufferName = 0;
+GLuint renderedTexture;
+
+unsigned int screen_VBO = 0;
 unsigned int VBO = 0;
 unsigned int draw_count = 0;
+
 using DrawData =
     std::tuple<unsigned int, unsigned int, glm::mat4, glm::mat4, glm::vec3>;
 using TextureData = std::tuple<unsigned int, int, int>;
@@ -33,11 +38,25 @@ std::vector<DrawData> draw_list;
 
 const float vertices[] = {
     // pos      // tex
-    0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 
+    1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 
+    1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f
+};
 
-    0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f};
+const float screen_vertices[] = {
+    // pos      // tex
+    -1.0f, 1.0f, 0.0f, 1.0f, 
+    1.0f, -1.0f, 1.0f, 0.0f, 
+    -1.0f, -1.0f, 0.0f, 0.0f,
+    -1.0f, 1.0f, 0.0f, 1.0f, 
+    1.0f, 1.0f, 1.0f, 1.0f, 
+    1.0f, -1.0f, 1.0f, 0.0f
+};
 
 Shader *shader = nullptr;
+Shader *shader_screen = nullptr;
+
 int viewport_x = 0;
 int viewport_y = 0;
 int viewport_w = kDefaultViewWidth;
@@ -60,7 +79,6 @@ bool Init() {
 
   std::string vertexCode;
   std::string fragmentCode;
-  std::string geometryCode;
   std::ifstream vertexShaderFile("data/shaders/sprite.vs");
   std::ifstream fragmentShaderFile("data/shaders/sprite.fs");
   std::stringstream vShaderStream, fShaderStream;
@@ -70,18 +88,71 @@ bool Init() {
   fragmentShaderFile.close();
   vertexCode = vShaderStream.str();
   fragmentCode = fShaderStream.str();
-
   shader = new Shader();
   shader->Compile(vertexCode.c_str(), fragmentCode.c_str());
 
-  glGenBuffers(1, &VBO);
+  std::string vertexCode1 = "";
+  std::string fragmentCode1 = "";
+  std::ifstream vertexShaderFile1("data/shaders/screen.vs");
+  std::ifstream fragmentShaderFile1("data/shaders/screen.fs");
+  std::stringstream vShaderStream1, fShaderStream1;
+  vShaderStream1 << vertexShaderFile1.rdbuf();
+  fShaderStream1 << fragmentShaderFile1.rdbuf();
+  vertexShaderFile1.close();
+  fragmentShaderFile1.close();
+  vertexCode1 = vShaderStream1.str();
+  fragmentCode1 = fShaderStream1.str();
+  shader_screen = new Shader();
+  shader_screen->Compile(vertexCode1.c_str(), fragmentCode1.c_str());
 
+  glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glDisableVertexAttribArray(0);
+
+  glGenBuffers(1, &screen_VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, screen_VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(screen_vertices), screen_vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  // ==========
+
+  // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
+  glGenFramebuffers(1, &FramebufferName);
+  glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+  // The texture we're going to render to
+  glGenTextures(1, &renderedTexture);
+
+  // "Bind" the newly created texture : all future texture functions will modify this texture
+  glBindTexture(GL_TEXTURE_2D, renderedTexture);
+
+  // Give an empty image to OpenGL ( the last "0" )
+  auto [w, h] = GetRenderSize();
+  glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, w, h, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+  // Poor filtering. Needed !
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+  // The depth buffer
+  GLuint depthrenderbuffer;
+  glGenRenderbuffers(1, &depthrenderbuffer);
+  glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, w, h);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
+
+  // Set "renderedTexture" as our colour attachement #0
+  glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
+
+  // Set the list of draw buffers.
+  GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+  glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+  
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  // Always check that our framebuffer is ok
+  if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        return false; 
+  // ==========
 
   lua::GStateModule("yapre")
       .Define<void (*)(const std::string &, int, int, int, int, int, float,
@@ -122,9 +193,6 @@ std::tuple<unsigned int, int, int> GetTextureId(std::string texture_filename) {
                GL_UNSIGNED_BYTE, tmp.get());
 
   stbi_image_free(data);
-
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
@@ -146,8 +214,8 @@ void DrawSprite(const std::string &texture_filename, glm::vec3 position,
     size.x = texture_w;
     size.y = texture_h;
   }
-  
-  position.z = position.z / 1048576;
+ 
+  position.z = position.z / (1024 * 1024);
   glm::mat4 model = glm::mat4(1.0f);
   model = glm::translate(model, position);
   model = glm::translate(
@@ -161,7 +229,7 @@ void DrawSprite(const std::string &texture_filename, glm::vec3 position,
   model = glm::scale(model, glm::vec3(size, 1.0f)); // last scale
   auto [w, h] = GetRenderSize();
   glm::mat4 projection = glm::ortho(0.0f, 1.f * w, 1.f * h, 0.0f, -1.0f, 1.0f);
-  unsigned int draw_id = position.z * 1024 * 1024 + draw_count;
+  unsigned int draw_id = position.z * 1024 * 1024 * 1024 * 1024 + draw_count;
   draw_count++;
 
   draw_list.emplace_back(
@@ -177,9 +245,15 @@ void DrawSprite(const std::string &texture_filename, int x, int y, int z,
 }
 
 void DrawAll() {
+  glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+  glViewport(0, 0, 320, 240);
+  glClearColor(.0f, .0f, .0f, 0.f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   draw_count = 0;
+
   shader->Use();
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
   glActiveTexture(GL_TEXTURE0);
   shader->SetInteger("sprite", 0);
@@ -199,6 +273,7 @@ void DrawAll() {
     glDrawArrays(GL_TRIANGLES, 0, 6);
   }
 
+  glDisableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   draw_list.clear();
 }
@@ -224,12 +299,27 @@ void RefreshViewport() {
 }
 
 void Update(int delta_ms) {
+  DrawAll();
+  
   RefreshViewport();
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
   glViewport(viewport_x, viewport_y, viewport_w, viewport_h);
   glClearColor(.2f, .2f, .2f, 0.f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  shader_screen->Use();
+  glBindBuffer(GL_ARRAY_BUFFER, screen_VBO);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  glActiveTexture(GL_TEXTURE0);
+  shader_screen->SetInteger("sprite", 0);
+  shader_screen->SetVector3f("spriteColor", glm::vec3(1.0f));
 
-  DrawAll();
+  glBindTexture(GL_TEXTURE_2D, renderedTexture);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+  glDisableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
   window::SwapWinodw();
 }
 
