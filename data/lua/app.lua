4b8f5c836007e91d42f45793b917c3bcbc91f88d
wo1fsea <wo1fsea@126.com>
2021-04-22 00:47:26 +0800

    add animation test

--- data/lua/app.lua
+++ data/lua/app.lua
@@ -11,6 +11,7 @@ local entity1 = nil
 local entity2 = nil
 local button = nil
 local text = nil
+local image = nil
 
 function app.Init()
     world1 = yecs.World:New("world1")
@@ -22,18 +23,51 @@ function app.Init()
     entity2 = yecs.Entity:New({"position", "sprite", "tree"})
     button = yecs.Entity:New({"position", "sprite", "input", "size", "tree", "tick"})
     text = yecs.Entity:New({"position", "sprite", "size", "text", "tick"})
+    image = yecs.Entity:New({"position", "sprite", "size", "tick"})
+    
+    image.sprite.sprites[1] = {
+        texture="data/image/animation/blood/1.png", 
+        color={r=1,g=1,b=1}, 
+        size={width=128, heigh=128}, 
+        offset={x=0, y=0, z=0},
+    }
+    image.position.x = 8
+    image.position.y = 240 - 8 - 128
 
+    image.tick:AddCallback(
+    "animation", 
+    function(self, ms)
+        if self.entity.animation_ms == nil then self.entity.animation_ms = 0 end
+
+        self.entity.animation_ms = self.entity.animation_ms + ms
+        if self.entity.animation_ms < 1000/24 then return end
+        self.entity.animation_ms = 0
+
+        if self.entity.animation_idx == nil or self.entity.animation_idx == 0 then 
+            self.entity.animation_idx = 1 
+            self.entity.animation_idx_d = 1 
+        end
+        if self.entity.animation_idx > 20 then 
+            self.entity.animation_idx = 20 
+        end
+        print(self.entity.animation_idx)
+        self.entity.sprite.sprites[1].texture= string.format("data/image/animation/blood/%d.png", self.entity.animation_idx)
+        self.entity.animation_idx = self.entity.animation_idx + self.entity.animation_idx_d
+    end, 
+    10
+    )
+    
     button.tick:AddCallback("a", function(self, ms) print(button, ms, 10) end, 10)
     text.tick:AddCallback("c", function(self, ms) print(text, ms, 3) end, 3)
 
     text.text:SetText("O wonder! \nHow many goodly creatures are there here!\nHow beauteous mankind is!\nO brave new world,\nThat has such people in't.")
-    text.position.x = 16
-    text.position.y = 32 
+    text.position.x = 8
+    text.position.y = 8 
 
     button.sprite.sprites[1] = {
         texture="data/image/ui/button16_0.png", 
         color={r=1,g=1,b=1}, 
-        size={width=64, heigh=64}, 
+        size={width=32, heigh=32}, 
         offset={x=0, y=0, z=0},
     }
     button.size.width = 64
@@ -44,9 +78,10 @@ function app.Init()
     end
     function button.input:OnTouchEnded(x, y)
         self.entity.sprite.sprites[1].texture = "data/image/ui/button16_0.png"
+        image.animation_idx = 1
     end
-    button.position.x = 256 
-    button.position.y = 128
+    button.position.x = 320 - 8 - 32 
+    button.position.y = 240 - 8 - 32
     button.position.z = 1
     
     entity2.tree:AddChild(entity1)
@@ -60,6 +95,7 @@ function app.Init()
     world1:AddEntity(entity2)
     world1:AddEntity(button)
     world1:AddEntity(text)
+    world1:AddEntity(image)
 end
 
 function app.Update(delta_ms)
