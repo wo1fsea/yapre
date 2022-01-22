f6310813c30cfd70f56f353c38a789f84860e4d7
quanyongh <wo1fsea@126.com>
2022-01-23 00:24:53 +0800

    mod: add centerx/y

--- data/lua/app/game/world_word_slide_maker.lua
+++ data/lua/app/game/world_word_slide_maker.lua
@@ -10,21 +10,31 @@ local core = require("core")
 local copy = require("copy")
 local yecs = core.yecs
 
+local panel_width = 320
+local panel_height = 240
+
 function world_word_slide:Make()
     local world = yecs.World:New("world_word_slide_" .. self.words)
-    world:AddSystemsByKeys({"sprite", "input", "tree", "tick"})
+    world:AddSystemsByKeys({"sprite", "input", "tree", "tick", "layout"})
+    
+    local canvas = yecs.EntityFactory:Make("panel")
+    world:AddEntity(canvas)
+    canvas:SetSize(panel_width, panel_height)
+
+    canvas.layout:SetCenterX(world:GetRoot().layout:GetCenterX(), 0)
+    canvas.layout:SetCenterY(world:GetRoot().layout:GetCenterY(), 0)
 
     local words = yecs.EntityFactory:Make("label")
     words:SetText(self.words)
 
     local rs = words:GetRenderSize()
     words.position = {
-        x = yapre.render_width / 2 - rs.width / 2,
-        y = yapre.render_height / 2 + rs.height,
+        x = panel_width / 2 - rs.width / 2,
+        y = panel_height / 2 + rs.height,
         z = 1
     }
 
-    world:AddEntity(words)
+    canvas.tree:AddChild(words)
 
     return world
 end
