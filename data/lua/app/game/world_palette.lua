23024ee679a323e453aa2e70f3804824a27c2370
wo1fsea <wo1fsea@126.com>
2022-01-24 19:28:57 +0800

    mod palette

--- data/lua/app/game/world_palette.lua
+++ data/lua/app/game/world_palette.lua
@@ -1,16 +1,16 @@
 local world_palette = {}
 
 local core = require("core")
-local copy = require("utils.copy")
 local yecs = core.yecs
 
 function world_palette:Make()
     local world = yecs.World:New("world_palette")
-    world:AddSystems({"sprite", "input", "tree", "tick"})
+    world:AddSystems({"sprite", "input", "tree", "tick", "layout"})
 
     local palette = yecs.EntityFactory:Make("palette")
     world:AddEntity(palette)
-    palette.position = {x=yapre.render_width/2-64, y=yapre.render_height/2, z=1}
+    palette.layout:SetCenterX(world:GetRoot().layout:GetCenterX(), 0)
+    palette.layout:SetCenterY(world:GetRoot().layout:GetCenterY(), 0)
 
     return world
 end
