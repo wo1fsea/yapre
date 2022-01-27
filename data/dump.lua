77919b9b86aca1e248f778377cd0d8d9aaae85af
wo1fsea <wo1fsea@126.com>
2022-01-27 19:13:53 +0800

    mod: ui

---
+++ data/dump.lua
@@ -0,0 +1,679 @@
+return {
+-- Table: {1}
+{
+   {2},
+   ["world_slides"]={3},
+},
+-- Table: {2}
+{
+   ["entities"]={4},
+   ["key"]="world_dungeon",
+   ["update_delta"]=0,
+   ["paused"]=true,
+   ["systems"]={5},
+   ["root_entity_key"]="9d2761cb",
+},
+-- Table: {3}
+{
+   ["entities"]={6},
+   ["key"]="world_slides",
+   ["update_delta"]=0,
+   ["paused"]=true,
+   ["systems"]={7},
+   ["root_entity_key"]="c875effb",
+},
+-- Table: {4}
+{
+   ["9d2761cb"]={8},
+   ["d9bdb5c4"]={9},
+   ["cb41d5aa"]={10},
+},
+-- Table: {5}
+{
+   "input",
+   "tree",
+   "sprite",
+   "tick",
+},
+-- Table: {6}
+{
+   ["85531afa"]={11},
+   ["2f460978"]={12},
+   ["c875effb"]={13},
+},
+-- Table: {7}
+{
+   "input",
+   "tree",
+   "sprite",
+   "tick",
+},
+-- Table: {8}
+{
+   ["components"]={14},
+   ["behavior_keys"]={15},
+   ["key"]="9d2761cb",
+},
+-- Table: {9}
+{
+   ["components"]={16},
+   ["behavior_keys"]={17},
+   ["key"]="d9bdb5c4",
+},
+-- Table: {10}
+{
+   ["components"]={18},
+   ["behavior_keys"]={19},
+   ["key"]="cb41d5aa",
+},
+-- Table: {11}
+{
+   ["components"]={20},
+   ["behavior_keys"]={21},
+   ["key"]="85531afa",
+},
+-- Table: {12}
+{
+   ["components"]={22},
+   ["behavior_keys"]={23},
+   ["key"]="2f460978",
+},
+-- Table: {13}
+{
+   ["components"]={24},
+   ["behavior_keys"]={25},
+   ["key"]="c875effb",
+},
+-- Table: {14}
+{
+   ["position"]={26},
+   ["size"]={27},
+   ["tree"]={28},
+   ["layout"]={29},
+},
+-- Table: {15}
+{
+},
+-- Table: {16}
+{
+   ["size"]={30},
+   ["tree"]={31},
+   ["position"]={32},
+   ["sprite"]={33},
+   ["input"]={34},
+   ["layout"]={35},
+},
+-- Table: {17}
+{
+   "joystick",
+   "world_dungeon_joystick_behavior",
+},
+-- Table: {18}
+{
+   ["tags"]={36},
+   ["tick"]={37},
+   ["data"]={38},
+   ["layout"]={39},
+   ["size"]={40},
+   ["position"]={41},
+   ["sprite"]={42},
+   ["tree"]={43},
+   ["animation"]={44},
+},
+-- Table: {19}
+{
+   "image",
+},
+-- Table: {20}
+{
+   ["size"]={45},
+   ["tree"]={46},
+   ["sprite"]={47},
+   ["tick"]={48},
+   ["data"]={49},
+   ["position"]={50},
+   ["input"]={51},
+   ["animation"]={52},
+},
+-- Table: {21}
+{
+   "button",
+   "world_slides_next_btn_behavior",
+},
+-- Table: {22}
+{
+   ["size"]={53},
+   ["tree"]={54},
+   ["sprite"]={55},
+   ["tick"]={56},
+   ["data"]={57},
+   ["position"]={58},
+   ["input"]={59},
+   ["animation"]={60},
+},
+-- Table: {23}
+{
+   "button",
+   "world_slides_prev_btn_behavior",
+},
+-- Table: {24}
+{
+   ["position"]={61},
+   ["size"]={62},
+   ["tree"]={63},
+   ["layout"]={64},
+},
+-- Table: {25}
+{
+},
+-- Table: {26}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=0,
+   ["key"]="position",
+},
+-- Table: {27}
+{
+   ["height"]=320,
+   ["width"]=320,
+   ["key"]="size",
+},
+-- Table: {28}
+{
+   ["children"]={65},
+},
+-- Table: {29}
+{
+   ["constraints"]={66},
+   ["key"]="layout",
+},
+-- Table: {30}
+{
+   ["height"]=64,
+   ["width"]=64,
+   ["key"]="size",
+},
+-- Table: {31}
+{
+   ["children"]={67},
+   ["parent"]="9d2761cb",
+},
+-- Table: {32}
+{
+   ["x"]=246,
+   ["y"]=110.0,
+   ["z"]=2,
+   ["key"]="position",
+},
+-- Table: {33}
+{
+   ["sprites"]={68},
+   ["key"]="sprite",
+},
+-- Table: {34}
+{
+   ["transparent"]=false,
+   ["touched"]=false,
+   ["key"]="input",
+},
+-- Table: {35}
+{
+   ["constraints"]={69},
+   ["key"]="layout",
+},
+-- Table: {36}
+{
+   ["dot"]=true,
+   ["key"]="tags",
+},
+-- Table: {37}
+{
+   ["key"]="tick",
+},
+-- Table: {38}
+{
+   ["speed"]={70},
+   ["key"]="data",
+},
+-- Table: {39}
+{
+   ["constraints"]={71},
+   ["key"]="layout",
+},
+-- Table: {40}
+{
+   ["height"]=0,
+   ["width"]=0,
+   ["key"]="size",
+},
+-- Table: {41}
+{
+   ["x"]=160.0,
+   ["y"]=120.0,
+   ["z"]=2,
+   ["key"]="position",
+},
+-- Table: {42}
+{
+   ["sprites"]={72},
+   ["key"]="sprite",
+},
+-- Table: {43}
+{
+   ["children"]={73},
+   ["parent"]="9d2761cb",
+},
+-- Table: {44}
+{
+   ["frame_rate"]=12,
+   ["waiting_timer"]=false,
+   ["next_idx"]={74},
+   ["animations"]={75},
+   ["key"]="animation",
+},
+-- Table: {45}
+{
+   ["height"]=32,
+   ["width"]=32,
+   ["key"]="size",
+},
+-- Table: {46}
+{
+   ["children"]={76},
+   ["parent"]="c875effb",
+},
+-- Table: {47}
+{
+   ["sprites"]={77},
+   ["key"]="sprite",
+},
+-- Table: {48}
+{
+   ["key"]="tick",
+},
+-- Table: {49}
+{
+   ["button_bind_key"]="x",
+   ["key"]="data",
+},
+-- Table: {50}
+{
+   ["x"]=284,
+   ["y"]=208,
+   ["z"]=1024,
+   ["key"]="position",
+},
+-- Table: {51}
+{
+   ["transparent"]=false,
+   ["touched"]=false,
+   ["key"]="input",
+},
+-- Table: {52}
+{
+   ["frame_rate"]=12,
+   ["waiting_timer"]=false,
+   ["next_idx"]={78},
+   ["animations"]={79},
+   ["key"]="animation",
+},
+-- Table: {53}
+{
+   ["height"]=32,
+   ["width"]=32,
+   ["key"]="size",
+},
+-- Table: {54}
+{
+   ["children"]={80},
+   ["parent"]="c875effb",
+},
+-- Table: {55}
+{
+   ["sprites"]={81},
+   ["key"]="sprite",
+},
+-- Table: {56}
+{
+   ["key"]="tick",
+},
+-- Table: {57}
+{
+   ["button_bind_key"]="z",
+   ["key"]="data",
+},
+-- Table: {58}
+{
+   ["x"]=252,
+   ["y"]=208,
+   ["z"]=1024,
+   ["key"]="position",
+},
+-- Table: {59}
+{
+   ["transparent"]=false,
+   ["touched"]=false,
+   ["key"]="input",
+},
+-- Table: {60}
+{
+   ["frame_rate"]=12,
+   ["waiting_timer"]=false,
+   ["next_idx"]={82},
+   ["animations"]={83},
+   ["key"]="animation",
+},
+-- Table: {61}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=0,
+   ["key"]="position",
+},
+-- Table: {62}
+{
+   ["height"]=320,
+   ["width"]=320,
+   ["key"]="size",
+},
+-- Table: {63}
+{
+   ["children"]={84},
+},
+-- Table: {64}
+{
+   ["constraints"]={85},
+   ["key"]="layout",
+},
+-- Table: {65}
+{
+   "d9bdb5c4",
+   "cb41d5aa",
+},
+-- Table: {66}
+{
+},
+-- Table: {67}
+{
+},
+-- Table: {68}
+{
+   ["joystick_head"]={86},
+   ["joystick_background"]={87},
+},
+-- Table: {69}
+{
+   ["h"]={88},
+   ["v"]={89},
+},
+-- Table: {70}
+{
+   ["x"]=0,
+   ["y"]=0,
+},
+-- Table: {71}
+{
+},
+-- Table: {72}
+{
+   ["image_border"]={90},
+   ["image"]={91},
+},
+-- Table: {73}
+{
+},
+-- Table: {74}
+{
+},
+-- Table: {75}
+{
+},
+-- Table: {76}
+{
+},
+-- Table: {77}
+{
+   ["button"]={92},
+},
+-- Table: {78}
+{
+},
+-- Table: {79}
+{
+   ["normal"]={93},
+   ["disabled"]={94},
+   ["pressed"]={95},
+},
+-- Table: {80}
+{
+},
+-- Table: {81}
+{
+   ["button"]={96},
+},
+-- Table: {82}
+{
+},
+-- Table: {83}
+{
+   ["normal"]={97},
+   ["disabled"]={98},
+   ["pressed"]={99},
+},
+-- Table: {84}
+{
+   "2f460978",
+   "85531afa",
+},
+-- Table: {85}
+{
+},
+-- Table: {86}
+{
+   ["size"]={100},
+   ["color"]={101},
+   ["offset"]={102},
+   ["texture"]="./image/ui/joystick32/4.png",
+},
+-- Table: {87}
+{
+   ["size"]={103},
+   ["color"]={104},
+   ["offset"]={105},
+   ["texture"]="./image/ui/joystick32/1.png",
+},
+-- Table: {88}
+{
+   "return function(e) return e.t_entity.layout:Right(e.is_parent)+e.offset-e.size.width end",
+   {106},
+},
+-- Table: {89}
+{
+   "return function(e) return e.t_entity.layout:Top(e.is_parent)+e.offset end",
+   {107},
+},
+-- Table: {90}
+{
+   ["size"]={108},
+   ["color"]={109},
+   ["offset"]={110},
+   ["texture"]="./image/ui/blank2.png",
+},
+-- Table: {91}
+{
+   ["size"]={111},
+   ["color"]={112},
+   ["offset"]={113},
+   ["texture"]="./image/ui/blank2.png",
+},
+-- Table: {92}
+{
+   ["size"]={114},
+   ["color"]={115},
+   ["offset"]={116},
+   ["texture"]="./image/ui/button16/1.png",
+},
+-- Table: {93}
+{
+   ["sprite_key"]="button",
+   ["texture_format"]="./image/ui/button16/%d.png",
+   ["end_idx"]=1,
+   ["start_idx"]=1,
+},
+-- Table: {94}
+{
+   ["sprite_key"]="button",
+   ["texture_format"]="./image/ui/button16/%d.png",
+   ["end_idx"]=3,
+   ["start_idx"]=3,
+},
+-- Table: {95}
+{
+   ["sprite_key"]="button",
+   ["texture_format"]="./image/ui/button16/%d.png",
+   ["end_idx"]=2,
+   ["start_idx"]=2,
+},
+-- Table: {96}
+{
+   ["size"]={117},
+   ["color"]={118},
+   ["offset"]={119},
+   ["texture"]="./image/ui/button16/1.png",
+},
+-- Table: {97}
+{
+   ["sprite_key"]="button",
+   ["texture_format"]="./image/ui/button16/%d.png",
+   ["end_idx"]=1,
+   ["start_idx"]=1,
+},
+-- Table: {98}
+{
+   ["sprite_key"]="button",
+   ["texture_format"]="./image/ui/button16/%d.png",
+   ["end_idx"]=3,
+   ["start_idx"]=3,
+},
+-- Table: {99}
+{
+   ["sprite_key"]="button",
+   ["texture_format"]="./image/ui/button16/%d.png",
+   ["end_idx"]=2,
+   ["start_idx"]=2,
+},
+-- Table: {100}
+{
+   ["height"]=64,
+   ["width"]=64,
+},
+-- Table: {101}
+{
+   ["r"]=1,
+   ["b"]=1,
+   ["g"]=1,
+},
+-- Table: {102}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=2,
+},
+-- Table: {103}
+{
+   ["height"]=64,
+   ["width"]=64,
+},
+-- Table: {104}
+{
+   ["r"]=1,
+   ["b"]=1,
+   ["g"]=1,
+},
+-- Table: {105}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=1,
+},
+-- Table: {106}
+{
+   ["offset"]=-10,
+   ["t_entity_key"]="9d2761cb",
+},
+-- Table: {107}
+{
+   ["offset"]=-10,
+   ["t_entity_key"]="cb41d5aa",
+},
+-- Table: {108}
+{
+   ["height"]=6,
+   ["width"]=6,
+},
+-- Table: {109}
+{
+   ["b"]=0.066666666666667,
+   ["a"]=1.0,
+   ["r"]=0.19607843137255,
+   ["g"]=0.0,
+},
+-- Table: {110}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=0,
+},
+-- Table: {111}
+{
+   ["height"]=2,
+   ["width"]=2,
+},
+-- Table: {112}
+{
+   ["r"]=1,
+   ["b"]=1,
+   ["g"]=1,
+},
+-- Table: {113}
+{
+   ["x"]=2,
+   ["y"]=2,
+   ["z"]=1,
+},
+-- Table: {114}
+{
+   ["height"]=32,
+   ["width"]=32,
+},
+-- Table: {115}
+{
+   ["r"]=1,
+   ["b"]=1,
+   ["g"]=1,
+},
+-- Table: {116}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=0,
+},
+-- Table: {117}
+{
+   ["height"]=32,
+   ["width"]=32,
+},
+-- Table: {118}
+{
+   ["r"]=1,
+   ["b"]=1,
+   ["g"]=1,
+},
+-- Table: {119}
+{
+   ["x"]=0,
+   ["y"]=0,
+   ["z"]=0,
+},
+}
