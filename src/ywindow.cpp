b2a480c3f836eb977388446c219c911730b73378
quanyongh <wo1fsea@126.com>
2022-02-19 16:34:28 +0800

    mod: window

--- src/ywindow.cpp
+++ src/ywindow.cpp
@@ -28,7 +28,7 @@ std::tuple<int, int> GetDrawableSize() {
   emscripten_webgl_get_drawing_buffer_size(context, &w, &h);
 #else
   if (mainWindow) {
-    SDL_GL_GetDrawableSize(mainWindow, &w, &h);
+    SDL_GetWindowSize(mainWindow, &w, &h);
   }
 #endif
   return std::make_tuple(w, h);
@@ -46,16 +46,17 @@ bool Init() {
   }
   atexit(SDL_Quit);
 
-  auto [w, h] = renderer::GetPreferRenderSize();
   SDL_SetHint(SDL_HINT_VIDEO_EXTERNAL_CONTEXT, "1");
+
+  auto [w, h] = renderer::GetPreferRenderSize();
   if (kFullScreen) {
-    mainWindow = SDL_CreateWindow(kWindowCaption, SDL_WINDOWPOS_UNDEFINED,
-                                  SDL_WINDOWPOS_UNDEFINED, 0, 0,
-                                  SDL_WINDOW_FULLSCREEN);
+    mainWindow =
+        SDL_CreateWindow(kWindowCaption, SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN);
   } else {
-    mainWindow = SDL_CreateWindow(kWindowCaption, SDL_WINDOWPOS_UNDEFINED,
-                                  SDL_WINDOWPOS_UNDEFINED, w, h,
-                                  SDL_WINDOW_RESIZABLE);
+    mainWindow =
+        SDL_CreateWindow(kWindowCaption, SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_RESIZABLE);
     // SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
   }
 
@@ -72,10 +73,7 @@ void Deinit() {
   SDL_Quit();
 }
 
-void SwapWinodw() {}
-
 void ResetWindowSize() {
-  return;
 #if defined(YPARE_WINDOWS) || defined(YPARE_MAC) || defined(YPARE_LINUX)
   if (mainWindow) {
     auto [w, h] = renderer::GetPreferRenderSize();
--- src/ywindow.h
+++ src/ywindow.h
@@ -4,12 +4,11 @@
 
 class SDL_Window;
 namespace yapre {
-namespace window{
+namespace window {
 extern SDL_Window *mainWindow;
 bool Init();
 void Deinit();
 std::tuple<int, int> GetDrawableSize();
-void SwapWinodw();
 void ResetWindowSize();
-} // namespace window_manager
+} // namespace window
 } // namespace yapre
