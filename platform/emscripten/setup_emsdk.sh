10f66afc6d36cb9b7c2d1c9571416aaee3a8cefe
wo1fsea <wo1fsea@126.com>
2023-04-05 10:17:50 +0800

    fix: emscripten build

--- platform/emscripten/setup_emsdk.sh
+++ platform/emscripten/setup_emsdk.sh
@@ -6,17 +6,23 @@ setup_emsdk()
         then
             mkdir ./tools
         fi
-
-        cd ./tools        
+        
+        cd ./tools
         git clone https://github.com/emscripten-core/emsdk.git
         
         cd ./emsdk
         git pull
-
-        ./emsdk install latest
-        ./emsdk activate latest
+        
+        # ./emsdk install latest
+        # ./emsdk activate latest
+        ./emsdk install 3.1.25
+        ./emsdk activate 3.1.25
+        
+        cd upstream/emscripten/cache/sysroot/include
+        mv ./SDL ./_SDL
+        cd ../../../../..
         
         cd ../..
-
+        
     fi
 }
